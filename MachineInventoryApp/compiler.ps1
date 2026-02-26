e7b9009a284812eccfbc3815728e36bc8f240d0a
Iago Carvalho <iagoo.feipe@gmail.com>
2026-02-26 01:11:31 -0300

    deleted:    MachineInventoryApp/a.ps1 	new file:   MachineInventoryApp/build/MachineInventoryApp.exe 	modified:   MachineInventoryApp/compiler.ps1 	new file:   MachineInventoryApp/source.xml 	modified:   MachineInventoryApp/src/views/appview.cpp 	new file:   MachineInventoryApp/test.ps1

--- MachineInventoryApp/compiler.ps1
+++ MachineInventoryApp/compiler.ps1
@@ -1,20 +1,60 @@
-Write-Output Compilando MachineInventoryApp...
+#!/usr/bin/env powershell
 
-cd src
-g++ -c controllers/appcontroller.cpp -o ../build/obj/appcontroller.o -I../include {wx-config --cxxflags}
-if %ERRORLEVEL% NEQ 0 goto :ERRO
+# PATH
+$PATH_BUILD = "build"
+$PATH_OBJ = "$PATH_BUILD/obj"
+$PATH_SOURCE = "src"
+$PATH_MINGW64 = "C:/msys64/mingw64"
 
-g++ -c models/appmodel.cpp -o ../build/obj/appmodel.o -I../include `wx-config --cxxflags`
-if %ERRORLEVEL% NEQ 0 goto :ERRO
+# FILE
+$FILE_EXE = "$PATH_BUILD/MachineInventoryApp.exe"
+$FILE_RSC = "$PATH_OBJ/resource.res"
 
-g++ -c views/appview.cpp -o ../build/obj/appview.o -I../include `wx-config --cxxflags`
-g++ -c views/app.cpp -o ../build/obj/app.o -I../include `wx-config --cxxflags`
-g++ -c views/forms/authform.cpp -o ../build/obj/authform.o -I../include `wx-config --cxxflags`
+# FLAGS
+$FLAGS_EXE = @("-mwindows", "-L$PATH_MINGW64/lib", "-lwx_mswu_xrc-3.2", "-lwx_mswu_html-3.2", "-lwx_mswu_qa-3.2", "-lwx_mswu_core-3.2", "-lwx_baseu_xml-3.2", "-lwx_baseu_net-3.2", "-lwx_baseu-3.2")
+$FLAGS_INCLUDE = @("-I$PATH_SOURCE", "-I$PATH_MINGW64/lib/wx/include/msw-unicode-3.2", "-I$PATH_MINGW64/include/wx-3.2", "-DWXUSINGDLL", "-D__WXMSW__")
 
-g++ -c main.cpp -o ../build/obj/main.o -I../include `wx-config --cxxflags`
+# Others
+$SOURCES = @("controllers/appcontroller.cpp", "models/appmodel.cpp", "views/appview.cpp", "views/app.cpp", "views/forms/authform.cpp", "main.cpp")
+$SOURCES_OUT = @("$PATH_OBJ/appcontroller.o", "$PATH_OBJ/appmodel.o", "$PATH_OBJ/appview.o", "$PATH_OBJ/app.o", "$PATH_OBJ/authform.o", "$PATH_OBJ/main.o")
 
-cd ../build/obj
-g++ resource.res appcontroller.o appmodel.o appview.o app.o authform.o main.o -o ../MachineInventoryApp.exe `wx-config --libs` -mwindows
+if (!(Test-Path $PATH_BUILD)) {
+    mkdir $PATH_BUILD
+}
 
-../MachineInventoryApp.exe
+if (!(Test-Path $PATH_OBJ)) {
+    mkdir $PATH_OBJ
+}
 
+if (!(Test-Path $FILE_RSC)) {
+    Copy-Item $PATH_SOURCE/resource.res $FILE_RSC
+}
+
+$build_exe_required = 0
+for ($i = 0; $i -lt $SOURCES.Count; $i++) {
+    $src = $PATH_SOURCE + "/" + $SOURCES[$i]
+    $out = $SOURCES_OUT[$i]
+    $out_last_write_time = ""
+
+    if (Test-Path $out) {
+        $out_last_write_time = (Get-Item $out).LastWriteTime
+    }
+
+    if ($out_last_write_time -gt (Get-Item $src).LastWriteTime) {
+        # Write-Output "$src up-to-date"
+        continue
+    }
+
+    $build_exe_required = 1
+    
+    Write-Output "building $src..."
+    g++ -c $src -o $out @FLAGS_INCLUDE
+}
+
+if ($build_exe_required -or !(Test-Path $PATH_OBJ)) {
+    g++ $FILE_RSC @SOURCES_OUT -o $FILE_EXE $FLAGS_EXE
+} else {
+    Write-Output "EXE up-to-date"
+}
+
+Start-Process $FILE_EXE
--- MachineInventoryApp/src/views/appview.cpp
+++ MachineInventoryApp/src/views/appview.cpp
@@ -10,6 +10,17 @@ inventory::AppView::AppView()
     : model(AppModel::getInstance())
     , window(new wxFrame(NULL, wxID_ANY, "Machine Inventory"))
 {
+    wxFont font = window->GetFont();
+    font.SetPointSize(11);
+    window->SetFont(font);
+
+    wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
+    sizer->AddStretchSpacer();
+    sizer->Add(new wxStaticText(window, wxID_ANY, "Test Compiler", wxDefaultPosition, wxDefaultSize, wxALIGN_CENTER_HORIZONTAL), 0, wxEXPAND);
+    sizer->AddStretchSpacer();
+
+    window->SetSizer(sizer);
+
     window->SetMinSize(wxSize(900, 600));
     window->SetBackgroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_FRAMEBK));
 	window->Center();
